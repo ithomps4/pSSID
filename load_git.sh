1d311d2a6337b99cd542fe48e0d91d9c48630ac2
thom1555 <thom1555@msu.edu>
2020-05-20 09:41:35 -0400

    Script to pull repo to devbox

---
+++ load_git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo 'Fetching git files'
+wget https://raw.githubusercontent.com/ithomps4/pSSID/master/api.py
+wget https://raw.githubusercontent.com/ithomps4/pSSID/master/pSSID_skeleton.json
+wget https://raw.githubusercontent.com/ithomps4/pSSID/master/parse.py
+wget https://raw.githubusercontent.com/ithomps4/pSSID/master/psjson.py
