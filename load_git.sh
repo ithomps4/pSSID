8756fc7aab80a7fba3af285803af9c4dd7f7272c
thom1555 <thom1555@msu.edu>
2020-05-21 02:08:12 -0400

    Added logger config to script

--- load_git.sh
+++ load_git.sh
@@ -5,3 +5,8 @@ wget https://raw.githubusercontent.com/ithomps4/pSSID/master/api.py
 wget https://raw.githubusercontent.com/ithomps4/pSSID/master/pSSID_skeleton.json
 wget https://raw.githubusercontent.com/ithomps4/pSSID/master/parse.py
 wget https://raw.githubusercontent.com/ithomps4/pSSID/master/psjson.py
+
+echo 'Configuring logger'
+wget https://raw.githubusercontent.com/ithomps4/pSSID/master/conf/pSSID-logger.conf
+sudo mv pSSID-logger.conf /etc/rsyslog.d/pSSID-logger.conf
+sudo service rsyslog restart
