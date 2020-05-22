5d4a9ede7061953d3b324b0cbfdd525b602669fb
thom1555 <thom1555@msu.edu>
2020-05-22 09:55:10 -0400

    Added iso time

--- queue/queue.py
+++ queue/queue.py
@@ -1,5 +1,7 @@
 import sched
 import time
+import iso8601 as iso
+import datetime
 
 s = sched.scheduler(time.time, time.sleep)
 
@@ -12,8 +14,10 @@ def high_freq():
     s.enter(2, 1, high_freq, argument=())
 
 def main():
-    s.enter(2, 1, high_freq, argument=())
-    s.enter(5, 2, low_freq, argument=())
+    sec_10 = (iso.iso8601_as_timedelta('PT10S')).total_seconds()
+    sec_5 = (iso.iso8601_as_timedelta('PT5S')).total_seconds()
+    s.enter(sec_5, 1, high_freq, argument=())
+    s.enter(sec_10, 2, low_freq, argument=())
     s.run()
     print('Exiting')
 
